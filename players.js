19cb4607d0cf2eab2c0186956faa81ea4a54c274
MiguelDiaz80s <gayahh123456789@gmail.com>
2026-08-04 22:54:38 +1000

    Create players.js

---
+++ players.js
@@ -0,0 +1,105 @@
+const players = {
+
+Australia: {
+
+"1930s": [
+{
+name:"Don Bradman",
+role:"Batter",
+batting:100,
+bowling:5,
+fielding:85
+}
+],
+
+"2000s": [
+{
+name:"Ricky Ponting",
+role:"Batter",
+batting:97,
+bowling:5,
+fielding:92
+}
+],
+
+"2020s": [
+{
+name:"Glenn Maxwell",
+role:"All-Rounder",
+batting:94,
+bowling:82,
+fielding:90
+},
+
+{
+name:"Pat Cummins",
+role:"Bowler",
+batting:50,
+bowling:95,
+fielding:90
+}
+]
+
+},
+
+
+India: {
+
+"1990s": [
+{
+name:"Sachin Tendulkar",
+role:"Batter",
+batting:99,
+bowling:20,
+fielding:90
+}
+],
+
+"2010s": [
+{
+name:"Virat Kohli",
+role:"Batter",
+batting:98,
+bowling:5,
+fielding:90
+}
+],
+
+"2020s": [
+{
+name:"Jasprit Bumrah",
+role:"Bowler",
+batting:15,
+bowling:99,
+fielding:85
+}
+]
+
+},
+
+
+England: {
+
+"1980s": [
+{
+name:"Ian Botham",
+role:"All-Rounder",
+batting:90,
+bowling:95,
+fielding:85
+}
+],
+
+"2020s": [
+{
+name:"Joe Root",
+role:"Batter",
+batting:95,
+bowling:40,
+fielding:88
+}
+]
+
+}
+
+};
