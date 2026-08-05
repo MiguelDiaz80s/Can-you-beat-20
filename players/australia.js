80ffcbe085e5d2e2d95317378becaa8948eb7ab9
MiguelDiaz80s <gayahh123456789@gmail.com>
2026-08-05 18:17:56 +1000

    Create australia.js

---
+++ players/australia.js
@@ -0,0 +1,1079 @@
+const australia = {
+
+"1900s": [
+
+{
+name:"Victor Trumper",
+role:"Batter",
+batting:96,
+bowling:10,
+fielding:90
+},
+{
+name:"Clem Hill",
+role:"Batter",
+batting:94,
+bowling:15,
+fielding:88
+},
+{
+name:"Monty Noble",
+role:"All-rounder",
+batting:88,
+bowling:80,
+fielding:85
+},
+{
+name:"Warwick Armstrong",
+role:"All-rounder",
+batting:90,
+bowling:88,
+fielding:85
+},
+{
+name:"Charlie Macartney",
+role:"All-rounder",
+batting:92,
+bowling:82,
+fielding:88
+},
+{
+name:"Jack Gregory",
+role:"All-rounder",
+batting:82,
+bowling:91,
+fielding:85
+},
+{
+name:"Bill Ponsford",
+role:"Batter",
+batting:94,
+bowling:10,
+fielding:86
+},
+{
+name:"Bert Oldfield",
+role:"Wicketkeeper",
+batting:82,
+bowling:5,
+fielding:95
+},
+{
+name:"Hugh Trumble",
+role:"Bowler",
+batting:60,
+bowling:90,
+fielding:85
+},
+{
+name:"George Giffen",
+role:"All-rounder",
+batting:88,
+bowling:86,
+fielding:82
+},
+{
+name:"Harry Trott",
+role:"Batter",
+batting:85,
+bowling:35,
+fielding:85
+}
+
+],
+
+"1910s": [
+
+{
+name:"Victor Trumper",
+role:"Batter",
+batting:96,
+bowling:10,
+fielding:90
+},
+{
+name:"Warwick Armstrong",
+role:"All-rounder",
+batting:91,
+bowling:88,
+fielding:85
+},
+{
+name:"Charlie Macartney",
+role:"All-rounder",
+batting:92,
+bowling:82,
+fielding:88
+},
+{
+name:"Clem Hill",
+role:"Batter",
+batting:94,
+bowling:15,
+fielding:88
+},
+{
+name:"Bert Oldfield",
+role:"Wicketkeeper",
+batting:82,
+bowling:5,
+fielding:95
+},
+{
+name:"Herbie Collins",
+role:"Batter",
+batting:88,
+bowling:20,
+fielding:85
+},
+{
+name:"Jack Gregory",
+role:"All-rounder",
+batting:83,
+bowling:91,
+fielding:85
+},
+{
+name:"Bill Whitty",
+role:"Bowler",
+batting:45,
+bowling:85,
+fielding:80
+},
+{
+name:"Arthur Mailey",
+role:"Bowler",
+batting:50,
+bowling:88,
+fielding:82
+},
+{
+name:"Charles Kelleway",
+role:"All-rounder",
+batting:80,
+bowling:70,
+fielding:84
+},
+{
+name:"Roy Minnett",
+role:"All-rounder",
+batting:78,
+bowling:75,
+fielding:80
+}
+
+],
+
+"1920s": [
+
+{
+name:"Don Bradman",
+role:"Batter",
+batting:100,
+bowling:25,
+fielding:92
+},
+{
+name:"Bill Ponsford",
+role:"Batter",
+batting:95,
+bowling:10,
+fielding:86
+},
+{
+name:"Stan McCabe",
+role:"Batter",
+batting:90,
+bowling:35,
+fielding:88
+},
+{
+name:"Clarrie Grimmett",
+role:"Bowler",
+batting:45,
+bowling:96,
+fielding:85
+},
+{
+name:"Bert Oldfield",
+role:"Wicketkeeper",
+batting:82,
+bowling:5,
+fielding:96
+},
+{
+name:"Vic Richardson",
+role:"All-rounder",
+batting:85,
+bowling:50,
+fielding:90
+},
+{
+name:"Alan Fairfax",
+role:"All-rounder",
+batting:80,
+bowling:82,
+fielding:80
+},
+{
+name:"Jack Fingleton",
+role:"Batter",
+batting:86,
+bowling:10,
+fielding:85
+},
+{
+name:"Chuck Fleetwood-Smith",
+role:"Bowler",
+batting:40,
+bowling:86,
+fielding:78
+},
+{
+name:"Tim Wall",
+role:"Bowler",
+batting:45,
+bowling:82,
+fielding:80
+},
+{
+name:"Bill Bowes",
+role:"Bowler",
+batting:40,
+bowling:85,
+fielding:82
+}
+
+]
+
+"1930s": [
+
+{
+name:"Don Bradman",
+role:"Batter",
+batting:100,
+bowling:25,
+fielding:92
+},
+{
+name:"Bill O'Reilly",
+role:"Bowler",
+batting:55,
+bowling:96,
+fielding:88
+},
+{
+name:"Stan McCabe",
+role:"Batter",
+batting:91,
+bowling:35,
+fielding:88
+},
+{
+name:"Bert Oldfield",
+role:"Wicketkeeper",
+batting:83,
+bowling:5,
+fielding:96
+},
+{
+name:"Bill Ponsford",
+role:"Batter",
+batting:95,
+bowling:10,
+fielding:86
+},
+{
+name:"Archie Jackson",
+role:"Batter",
+batting:90,
+bowling:10,
+fielding:85
+},
+{
+name:"Chuck Fleetwood-Smith",
+role:"Bowler",
+batting:40,
+bowling:86,
+fielding:78
+},
+{
+name:"Ernie McCormick",
+role:"Bowler",
+batting:45,
+bowling:84,
+fielding:80
+},
+{
+name:"Tim Wall",
+role:"Bowler",
+batting:45,
+bowling:82,
+fielding:80
+},
+{
+name:"Leo O'Brien",
+role:"Batter",
+batting:82,
+bowling:10,
+fielding:80
+},
+{
+name:"Alan Fairfax",
+role:"All-rounder",
+batting:80,
+bowling:82,
+fielding:80
+}
+
+],
+
+"1940s": [
+
+{
+name:"Don Bradman",
+role:"Batter",
+batting:100,
+bowling:25,
+fielding:92
+},
+{
+name:"Keith Miller",
+role:"All-rounder",
+batting:93,
+bowling:94,
+fielding:90
+},
+{
+name:"Ray Lindwall",
+role:"Bowler",
+batting:65,
+bowling:96,
+fielding:88
+},
+{
+name:"Arthur Morris",
+role:"Batter",
+batting:92,
+bowling:15,
+fielding:88
+},
+{
+name:"Neil Harvey",
+role:"Batter",
+batting:91,
+bowling:20,
+fielding:90
+},
+{
+name:"Don Tallon",
+role:"Wicketkeeper",
+batting:80,
+bowling:5,
+fielding:95
+},
+{
+name:"Bill Brown",
+role:"Batter",
+batting:85,
+bowling:10,
+fielding:82
+},
+{
+name:"Lindsay Hassett",
+role:"Batter",
+batting:87,
+bowling:20,
+fielding:85
+},
+{
+name:"Colin McCool",
+role:"All-rounder",
+batting:80,
+bowling:75,
+fielding:82
+},
+{
+name:"Ernie Toshack",
+role:"Bowler",
+batting:35,
+bowling:84,
+fielding:78
+},
+{
+name:"Doug Ring",
+role:"All-rounder",
+batting:75,
+bowling:78,
+fielding:80
+}
+
+],
+
+"1950s": [
+
+{
+name:"Richie Benaud",
+role:"All-rounder",
+batting:82,
+bowling:92,
+fielding:90
+},
+{
+name:"Neil Harvey",
+role:"Batter",
+batting:93,
+bowling:20,
+fielding:90
+},
+{
+name:"Alan Davidson",
+role:"All-rounder",
+batting:85,
+bowling:95,
+fielding:92
+},
+{
+name:"Ray Lindwall",
+role:"Bowler",
+batting:65,
+bowling:96,
+fielding:88
+},
+{
+name:"Keith Miller",
+role:"All-rounder",
+batting:93,
+bowling:94,
+fielding:90
+},
+{
+name:"Don Tallon",
+role:"Wicketkeeper",
+batting:80,
+bowling:5,
+fielding:95
+},
+{
+name:"Ian Johnson",
+role:"All-rounder",
+batting:75,
+bowling:82,
+fielding:85
+},
+{
+name:"Colin McDonald",
+role:"Batter",
+batting:86,
+bowling:10,
+fielding:85
+},
+{
+name:"Peter Burge",
+role:"Batter",
+batting:84,
+bowling:15,
+fielding:82
+},
+{
+name:"Jim Burke",
+role:"Batter",
+batting:83,
+bowling:10,
+fielding:82
+},
+{
+name:"Jack Wilson",
+role:"Bowler",
+batting:40,
+bowling:80,
+fielding:78
+}
+
+],
+  
+
+  "1960s": [
+
+{
+name:"Bob Simpson",
+role:"Batter",
+batting:90,
+bowling:40,
+fielding:90
+},
+{
+name:"Richie Benaud",
+role:"All-rounder",
+batting:82,
+bowling:92,
+fielding:90
+},
+{
+name:"Bill Lawry",
+role:"Batter",
+batting:88,
+bowling:10,
+fielding:85
+},
+{
+name:"Doug Walters",
+role:"All-rounder",
+batting:90,
+bowling:70,
+fielding:85
+},
+{
+name:"Ian Chappell",
+role:"Batter",
+batting:92,
+bowling:20,
+fielding:90
+},
+{
+name:"Graham McKenzie",
+role:"Bowler",
+batting:45,
+bowling:88,
+fielding:82
+},
+{
+name:"Ashley Mallett",
+role:"Bowler",
+batting:40,
+bowling:82,
+fielding:80
+},
+{
+name:"Brian Taber",
+role:"Wicketkeeper",
+batting:75,
+bowling:5,
+fielding:92
+},
+{
+name:"Alan Connolly",
+role:"Bowler",
+batting:40,
+bowling:84,
+fielding:80
+},
+{
+name:"Bob Cowper",
+role:"Batter",
+batting:85,
+bowling:20,
+fielding:82
+},
+{
+name:"Keith Stackpole",
+role:"Batter",
+batting:84,
+bowling:25,
+fielding:85
+}
+
+],
+
+"1970s": [
+
+{
+name:"Greg Chappell",
+role:"Batter",
+batting:97,
+bowling:25,
+fielding:92
+},
+{
+name:"Ian Chappell",
+role:"Batter",
+batting:92,
+bowling:20,
+fielding:90
+},
+{
+name:"Dennis Lillee",
+role:"Bowler",
+batting:40,
+bowling:99,
+fielding:88
+},
+{
+name:"Jeff Thomson",
+role:"Bowler",
+batting:45,
+bowling:97,
+fielding:85
+},
+{
+name:"Rod Marsh",
+role:"Wicketkeeper",
+batting:85,
+bowling:5,
+fielding:96
+},
+{
+name:"Doug Walters",
+role:"All-rounder",
+batting:90,
+bowling:70,
+fielding:85
+},
+{
+name:"Max Walker",
+role:"Bowler",
+batting:50,
+bowling:82,
+fielding:80
+},
+{
+name:"Ashley Mallett",
+role:"Bowler",
+batting:45,
+bowling:84,
+fielding:80
+},
+{
+name:"Gary Gilmour",
+role:"All-rounder",
+batting:75,
+bowling:85,
+fielding:85
+},
+{
+name:"Kerry O'Keeffe",
+role:"Bowler",
+batting:40,
+bowling:82,
+fielding:80
+},
+{
+name:"Rick McCosker",
+role:"Batter",
+batting:82,
+bowling:10,
+fielding:85
+}
+
+],
+
+"1980s": [
+
+{
+name:"Allan Border",
+role:"Batter",
+batting:94,
+bowling:30,
+fielding:92
+},
+{
+name:"Greg Chappell",
+role:"Batter",
+batting:97,
+bowling:25,
+fielding:92
+},
+{
+name:"Dennis Lillee",
+role:"Bowler",
+batting:40,
+bowling:99,
+fielding:88
+},
+{
+name:"Rod Marsh",
+role:"Wicketkeeper",
+batting:85,
+bowling:5,
+fielding:96
+},
+{
+name:"David Boon",
+role:"Batter",
+batting:90,
+bowling:10,
+fielding:85
+},
+{
+name:"Geoff Lawson",
+role:"Bowler",
+batting:45,
+bowling:88,
+fielding:82
+},
+{
+name:"Merv Hughes",
+role:"Bowler",
+batting:55,
+bowling:90,
+fielding:85
+},
+{
+name:"Steve Waugh",
+role:"All-rounder",
+batting:88,
+bowling:75,
+fielding:90
+},
+{
+name:"Dean Jones",
+role:"Batter",
+batting:88,
+bowling:15,
+fielding:88
+},
+{
+name:"Craig McDermott",
+role:"Bowler",
+batting:45,
+bowling:90,
+fielding:82
+},
+{
+name:"Mike Whitney",
+role:"Bowler",
+batting:35,
+bowling:82,
+fielding:80
+}
+
+],
+
+  "1990s": [
+
+{
+name:"Shane Warne",
+role:"Bowler",
+batting:55,
+bowling:99,
+fielding:90
+},
+{
+name:"Glenn McGrath",
+role:"Bowler",
+batting:40,
+bowling:98,
+fielding:90
+},
+{
+name:"Mark Waugh",
+role:"Batter",
+batting:91,
+bowling:45,
+fielding:94
+},
+{
+name:"Steve Waugh",
+role:"All-rounder",
+batting:92,
+bowling:75,
+fielding:92
+},
+{
+name:"Allan Border",
+role:"Batter",
+batting:94,
+bowling:30,
+fielding:92
+},
+{
+name:"Ian Healy",
+role:"Wicketkeeper",
+batting:84,
+bowling:5,
+fielding:96
+},
+{
+name:"David Boon",
+role:"Batter",
+batting:90,
+bowling:10,
+fielding:85
+},
+{
+name:"Michael Slater",
+role:"Batter",
+batting:88,
+bowling:5,
+fielding:85
+},
+{
+name:"Damien Martyn",
+role:"Batter",
+batting:87,
+bowling:20,
+fielding:90
+},
+{
+name:"Craig McDermott",
+role:"Bowler",
+batting:45,
+bowling:90,
+fielding:82
+},
+{
+name:"Jason Gillespie",
+role:"Bowler",
+batting:55,
+bowling:90,
+fielding:88
+}
+
+],
+
+"2000s": [
+
+{
+name:"Ricky Ponting",
+role:"Batter",
+batting:98,
+bowling:20,
+fielding:95
+},
+{
+name:"Adam Gilchrist",
+role:"Wicketkeeper",
+batting:96,
+bowling:10,
+fielding:98
+},
+{
+name:"Matthew Hayden",
+role:"Batter",
+batting:95,
+bowling:15,
+fielding:85
+},
+{
+name:"Shane Warne",
+role:"Bowler",
+batting:55,
+bowling:99,
+fielding:90
+},
+{
+name:"Glenn McGrath",
+role:"Bowler",
+batting:40,
+bowling:98,
+fielding:90
+},
+{
+name:"Brett Lee",
+role:"Bowler",
+batting:65,
+bowling:96,
+fielding:88
+},
+{
+name:"Andrew Symonds",
+role:"All-rounder",
+batting:88,
+bowling:80,
+fielding:96
+},
+{
+name:"Michael Clarke",
+role:"Batter",
+batting:91,
+bowling:50,
+fielding:90
+},
+{
+name:"Jason Gillespie",
+role:"Bowler",
+batting:55,
+bowling:90,
+fielding:88
+},
+{
+name:"Justin Langer",
+role:"Batter",
+batting:88,
+bowling:10,
+fielding:85
+},
+{
+name:"Damien Martyn",
+role:"Batter",
+batting:87,
+bowling:20,
+fielding:90
+}
+
+],
+
+"2010s": [
+
+{
+name:"Steve Smith",
+role:"Batter",
+batting:98,
+bowling:30,
+fielding:92
+},
+{
+name:"David Warner",
+role:"Batter",
+batting:95,
+bowling:10,
+fielding:90
+},
+{
+name:"Mitchell Starc",
+role:"Bowler",
+batting:70,
+bowling:96,
+fielding:90
+},
+{
+name:"Michael Clarke",
+role:"Batter",
+batting:91,
+bowling:50,
+fielding:90
+},
+{
+name:"Glenn Maxwell",
+role:"All-rounder",
+batting:94,
+bowling:85,
+fielding:95
+},
+{
+name:"Nathan Lyon",
+role:"Bowler",
+batting:55,
+bowling:90,
+fielding:88
+},
+{
+name:"Josh Hazlewood",
+role:"Bowler",
+batting:50,
+bowling:92,
+fielding:88
+},
+{
+name:"Mitchell Johnson",
+role:"Bowler",
+batting:65,
+bowling:96,
+fielding:90
+},
+{
+name:"Brad Haddin",
+role:"Wicketkeeper",
+batting:85,
+bowling:5,
+fielding:94
+},
+{
+name:"Aaron Finch",
+role:"Batter",
+batting:88,
+bowling:10,
+fielding:85
+},
+{
+name:"Shane Watson",
+role:"All-rounder",
+batting:88,
+bowling:82,
+fielding:90
+}
+
+],"2020s": [
+
+{
+name:"Pat Cummins",
+role:"Bowler",
+batting:75,
+bowling:96,
+fielding:92
+},
+
+{
+name:"Steve Smith",
+role:"Batter",
+batting:98,
+bowling:30,
+fielding:92
+},
+
+{
+name:"David Warner",
+role:"Batter",
+batting:95,
+bowling:10,
+fielding:90
+},
+
+{
+name:"Travis Head",
+role:"Batter",
+batting:93,
+bowling:25,
+fielding:88
+},
+
+{
+name:"Mitchell Starc",
+role:"Bowler",
+batting:70,
+bowling:96,
+fielding:90
+},
+
+{
+name:"Josh Hazlewood",
+role:"Bowler",
+batting:50,
+bowling:93,
+fielding:88
+},
+
+{
+name:"Glenn Maxwell",
+role:"All-rounder",
+batting:94,
+bowling:85,
+fielding:96
+},
+
+{
+name:"Marnus Labuschagne",
+role:"Batter",
+batting:90,
+bowling:20,
+fielding:88
+},
+
+{
+name:"Alex Carey",
+role:"Wicketkeeper",
+batting:82,
+bowling:5,
+fielding:94
+},
+
+{
+name:"Cameron Green",
+role:"All-rounder",
+batting:86,
+bowling:82,
+fielding:90
+},
+
+{
+name:"Nathan Lyon",
+role:"Bowler",
+batting:55,
+bowling:91,
+fielding:88
+}
+
+]
+
+};
+
